2cdf368c648567960a6819498865c8a2bb56aec9
sanket-k <sanket0249@gmail.com>
2017-11-20 11:48:21 +0530

    complete(other than payable function) -v0.0.6

--- lease.sol
+++ lease.sol
@@ -1,6 +1,6 @@
 /**
  *                                  Lease - Smart Contract
- *          1. Contract Initializer has admin access 
+ *          1. Contract Initializer has admin access
  *          2. Only admin can add an Issuer
  *          3. Only Issuer can sign a lease
  *          4. Any one can make a lease
@@ -11,15 +11,15 @@ pragma solidity ^0.4.11;
 
 contract leaseIssue {
     address admin;         //user with higest rights
-    
+
     //stores all values of an Issuer using an unique identifier (here it is the address of the issuer)
     struct IssuerInformation{
-        string IssuerNmae;
+        string IssuerName;
         string IssuerDetails;
         address IssuerAddress;
     }
-    
-    //stores all values of a lease using a specific identifier 
+
+    //stores all values of a lease using a specific identifier
     struct lesseInformation{
         string from;
         address fromAddress;
@@ -34,57 +34,53 @@ contract leaseIssue {
         address Issuer2Address;
         address Issuer3Address;
     }
-    
+
     mapping (address => IssuerInformation) IssuerInfo;      // maps to the structs using a specific identifier (here it is address)
-    
-    mapping (bytes32 => lesseInformation) leaseInfo;                       // maps to the structs using a specific identifier (here it is bytes32 or UIN)    
-    
+
+    mapping (bytes32 => lesseInformation) leaseInfo;                       // maps to the structs using a specific identifier (here it is bytes32 or UIN)
+
     //Constructor(to initialize admin)
     function leaseIssue() public{
         admin = msg.sender;
     }
-    
+
     //Modifier to allow only admin to certain sections of code
     modifier OnlyAdmin(){
         require(msg.sender == admin);
         _;
     }
-    
+
     //Modifier to allow only an Issuer to some sections of the code
     modifier OnlyIssuer(){
         require(msg.sender == IssuerInfo[msg.sender].IssuerAddress);
         _;
     }
-    
-    function compare(bytes32 _UIN) external returns(bool){
-        if(IssuerInfo[_UIN].IssuerState1 == true && IssuerInfo[_UIN].IssuerState2 == true){
-            return(true);
-        }
-    }
-    
-    // modifier approvedTrue(){
-    //     require(IssuerState1)
-    // }
 
-    
+    //modifier to check if the lease has been approved
+    modifier approvedTrue(bytes32 _UIN){
+        require(leaseInfo[_UIN].IssuerState1 == true && leaseInfo[_UIN].IssuerState2 == true && leaseInfo[_UIN].IssuerState3);
+        _;
+    }
+
+
     //function to add issuer who issues the lease
-    function addIssuer(address _address,
-                        string _IssuerNmae,
-                        string _IssuerDetails)external OnlyAdmin returns(bool){
-                            
-        IssuerInfo[_address].IssuerNmae = _IssuerNmae;
+    function addIssuer(string _IssuerName,
+                        string _IssuerDetails,
+                        address _address)external OnlyAdmin returns(bool){
+
+        IssuerInfo[_address].IssuerName = _IssuerName;
         IssuerInfo[_address].IssuerDetails = _IssuerDetails;
         IssuerInfo[_address].IssuerAddress = _address;
     }
-    
+
     //function to display the issuer details(name and details)
     function IssuerInfoDetails(address _address) external constant returns(string, string){
         return(
-                IssuerInfo[_address].IssuerNmae,
+                IssuerInfo[_address].IssuerName,
                 IssuerInfo[_address].IssuerDetails);
     }
-    
-    //function to make a lease between two entities 
+
+    //function to make a lease between two entities
     function makeLease(bytes32 _UIN,
                         string _from,
                         string _to,
@@ -98,22 +94,22 @@ contract leaseIssue {
         leaseInfo[_UIN].addenda = _addenda;
         leaseInfo[_UIN].date = _date;
     }
-    
+
     function approve1(bytes32 _UIN) OnlyIssuer external returns(bool) {
         leaseInfo[_UIN].IssuerState1 = true;
         leaseInfo[_UIN].Issuer1Address = msg.sender;
     }
-    
+
     function approve2(bytes32 _UIN) OnlyIssuer external returns(bool) {
         leaseInfo[_UIN].IssuerState2 = true;
         leaseInfo[_UIN].Issuer2Address = msg.sender;
     }
-    
+
     function approve3(bytes32 _UIN) OnlyIssuer external returns(bool) {
         leaseInfo[_UIN].IssuerState3 = true;
         leaseInfo[_UIN].Issuer3Address = msg.sender;
     }
-    
+
     //function to display the nature of the lease
     function displayLease(bytes32 _UIN) external constant returns(string,
                                                                   address,
@@ -129,12 +125,14 @@ contract leaseIssue {
                 leaseInfo[_UIN].addenda,
                 leaseInfo[_UIN].date);
     }
-    
-    function displayApproved(bytes32 _UIN) external constant returns(address, address, address) {
+
+    //function to display the status of the lease once all the issuers have approved it, the display option is available.
+    function displayApproved(bytes32 _UIN) approvedTrue(_UIN) external constant returns(address, address, address, string) {
         return(
                 leaseInfo[_UIN].Issuer1Address,
                 leaseInfo[_UIN].Issuer2Address,
-                leaseInfo[_UIN].Issuer3Address);
+                leaseInfo[_UIN].Issuer3Address,
+                "approved");
     }
-    
+
 }
